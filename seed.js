5507e3ac1b1b0a77502ecfd040bf8ff2d80eec12
Kelsey <kelseysroberts@gmail.com>
2015-07-27 15:16:05 -0400

    some seed data and model edits

--- seed.js
+++ seed.js
@@ -1,18 +1,37 @@
+var async = require('async'),
+    mongoose = require('mongoose');
+
+var models = require('/server/db/models');
+var Product = models.Product;
+
 var data = {
-    Hotel: [
-        {name: "Andaz Wall Street", place: [new Place({address: "75 Wall St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705137, -74.007624]})], num_stars: 4, amenities: "Pool, Free Wi-Fi" },
-        {name: "Hotel Mulberry", place: [new Place({address: "52 Mulberry St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715317, -73.999542]})], num_stars: 4.5, amenities: "Free Wi-Fi" },
-        {name: "The Ritz-Carlton New York, Battery Park", place: [new Place({address: "Two West Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705417, -74.017241]})], num_stars: 4.5, amenities: "24 hour Gym, Paid Wi-Fi" },
-        {name: "Wall Street Inn", place: [new Place({address: "9 S William St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.704581, -74.010273]})], num_stars: 4, amenities: "Free Wi-Fi" },
-        {name: "Smyth TriBeCa", place: [new Place({address: "85 West Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715076, -74.009301]})], num_stars: 3.5, amenities: "24 hour Gym" },
-        {name: "Double Tree", place: [new Place({address: "8 Stone St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.703961, -74.012350]})], num_stars: 3.5, amenities: "24 hour Gym" },
-        {name: "Hotel 91", place: [new Place({address: "91 E Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.713427, -73.993624]})], num_stars: 3.5, amenities: "Free Wi-Fi" },
-        {name: "Conrad New York Hotel", place: [new Place({address: "102 N End Ave", city: "New York", state: "NY", phone: "123-456-7890", location: [40.714985, -74.015841]})], num_stars: 3.5, amenities: "Paid Wi-Fi, Dogs Allowed" },
-        {name: "Millenium Hilton Hotel", place: [new Place({address: "55 Church St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.711597, -74.010392]})], num_stars: 3.5, amenities: "Paid Wi-Fi" },
-        {name: "US Pacific Hotel", place: [new Place({address: "106 Bowery", city: "New York", state: "NY", phone: "123-456-7890", location: [40.717873, -73.995231]})], num_stars: 2, amenities: "Accepts Credit Cards" },
-        {name: "Gild Hall, a Thompson Hotel", place: [new Place({address: "15 Gold Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.707894, -74.007108]})], num_stars: 4, amenities: "Paid Wi-Fi" },
-        {name: "W New York", place: [new Place({address: "123 Washington Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709102, -74.013997]})], num_stars: 3.5, amenities: "Pool, 24 hour Gym, Paid Wi-Fi" },
-        {name: "New York Marriott Downtown", place: [new Place({address: "85 W St at Albany St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.731131, -73.989568]})], num_stars: 3.5, amenities: "24 hour Gym, Paid Wi-Fi" },
-        {name: "Cosmopolitan Hotel", place: [new Place({address: "95 W Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715681, -74.008922]})], num_stars: 3.5, amenities: "Free Wif-Fi" },
-        {name: "Club Quarters", place: [new Place({address: "140 Washington St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709630, -74.013940]})], num_stars: 4, amenities: "Free Wif-Fi" }
-    ],
+    Product: [
+        {name: "Metropolis Coffee", company: "Hipster Coffee Comp", description: "This unique cognac-like blend balances delicacy and elegance, with jasmine and orange notes giving way to buttery shortbread, and a mild caramel that ties it all together. A light and refreshing spring and summer brew with subtle yet astonishing nuances.", price: 15.30, photo: "/metropolis.jpg", rating: 3, category: "Coffee", roast: "Medium", region: "Africa"},
+        {name: "Perc Coffee", company: "Koke Cooperative Ethiopia", description: "Koke hails from Chyalalcktu village in the Kochere District of Southern Oromia. This coffee is special to us because it marks the first hybrid process Ethiopia we’ve ever offered. It’s the perfect blend of a washed and natural processed coffee. ", price: 14, photo: "/perc.jpeg", rating: 3, category: "Coffee", roast: "Medium", region: "Africa"},
+        {name: "San Jose OCAÑA", company: "Cuvee Coffee", description: "This is not a loud coffee, a fruit bomb, it’s something more. Few farms in the world, let alone Guatemala are able to produce coffee of this quality this consistently. ", price: 13.95, photo: "/sanJoseOcana.jpeg", rating: 3, category: "Coffee", roast: "Medium", region: "Central America"},
+        {name: "Yum Coffee", company: "Coughfay Companty", description: "YUMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM ", price: 25, photo: "/sanJoseOcana.jpeg", rating: 3, category: "Coffee", roast: "Bold", region: "Mars"},
+        {name: "Coffee Me", company: "KAKAKACoffee", description: "Great for kids!", price: 10, photo: "/sanJoseOcana.jpeg", rating: 3, category: "Coffee", roast: "Light", region: "Chucky Cheese"}
+    ]}
+
+
+
+mongoose.connection.on('open', function() {
+mongoose.connection.db.dropDatabase(function() {
+    
+    console.log("Dropped old data, now inserting data");
+    async.each(Object.keys(data),
+        function(modelName, outerDone) {
+            async.each(data[modelName],
+                function(d, innerDone) {
+                    models[modelName].create(d, innerDone);
+                },
+                outerDone
+            );
+        },
+        function(err) {
+            console.log("Finished inserting data");
+            console.log("Control-C to quit");
+        }
+    );
+});
+});
---
+++ server/db/models/productModel.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+var product = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	company: {
+		type: String
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	photo: {
+		data: Buffer,
+		contentType: String
+	},
+
+	rating: {
+		type: Number,
+		min: 1,
+		max: 5
+	},
+
+	category: {
+		type: String,
+		required: true
+	},
+
+	roast: {
+		type: String
+	},
+
+	region: {
+		type: String
+	},
+	price: {
+		type: Number
+	}
+
+
+})
+
+
+mongoose.model('Product', product);
