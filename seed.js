89dd42a3cd07827677a7ad1067b89026d7fea6cd
Kelsey <kelseysroberts@gmail.com>
2015-07-27 12:38:43 -0400

    temp commit

--- seed.js
+++ seed.js
@@ -1,58 +1,18 @@
-/*
-
-This seed file is only a placeholder. It should be expanded and altered
-to fit the development of your application.
-
-It uses the same file the server uses to establish
-the database connection:
---- server/db/index.js
-
-The name of the database used is set in your environment files:
---- server/env/*
-
-This seed file has a safety check to see if you already have users
-in the database. If you are developing multiple applications with the
-fsg scaffolding, keep in mind that fsg always uses the same database
-name in the environment files.
-
-*/
-
-var mongoose = require('mongoose');
-var Promise = require('bluebird');
-var chalk = require('chalk');
-var User = Promise.promisifyAll(mongoose.model('User'));
-var connectToDb = require('./server/db');
-
-var seedUsers = function () {
-
-    var users = [
-        {
-            email: 'testing@fsa.com',
-            password: 'password'
-        },
-        {
-            email: 'obama@gmail.com',
-            password: 'potus'
-        }
-    ];
-
-    return User.createAsync(users);
-
-};
-
-connectToDb.then(function () {
-    User.findAsync({}).then(function (users) {
-        if (users.length === 0) {
-            return seedUsers();
-        } else {
-            console.log(chalk.magenta('Seems to already be user data, exiting!'));
-            process.kill(0);
-        }
-    }).then(function () {
-        console.log(chalk.green('Seed successful!'));
-        process.kill(0);
-    }).catch(function (err) {
-        console.error(err);
-        process.kill(1);
-    });
-});
+var data = {
+    Hotel: [
+        {name: "Andaz Wall Street", place: [new Place({address: "75 Wall St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705137, -74.007624]})], num_stars: 4, amenities: "Pool, Free Wi-Fi" },
+        {name: "Hotel Mulberry", place: [new Place({address: "52 Mulberry St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715317, -73.999542]})], num_stars: 4.5, amenities: "Free Wi-Fi" },
+        {name: "The Ritz-Carlton New York, Battery Park", place: [new Place({address: "Two West Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705417, -74.017241]})], num_stars: 4.5, amenities: "24 hour Gym, Paid Wi-Fi" },
+        {name: "Wall Street Inn", place: [new Place({address: "9 S William St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.704581, -74.010273]})], num_stars: 4, amenities: "Free Wi-Fi" },
+        {name: "Smyth TriBeCa", place: [new Place({address: "85 West Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715076, -74.009301]})], num_stars: 3.5, amenities: "24 hour Gym" },
+        {name: "Double Tree", place: [new Place({address: "8 Stone St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.703961, -74.012350]})], num_stars: 3.5, amenities: "24 hour Gym" },
+        {name: "Hotel 91", place: [new Place({address: "91 E Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.713427, -73.993624]})], num_stars: 3.5, amenities: "Free Wi-Fi" },
+        {name: "Conrad New York Hotel", place: [new Place({address: "102 N End Ave", city: "New York", state: "NY", phone: "123-456-7890", location: [40.714985, -74.015841]})], num_stars: 3.5, amenities: "Paid Wi-Fi, Dogs Allowed" },
+        {name: "Millenium Hilton Hotel", place: [new Place({address: "55 Church St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.711597, -74.010392]})], num_stars: 3.5, amenities: "Paid Wi-Fi" },
+        {name: "US Pacific Hotel", place: [new Place({address: "106 Bowery", city: "New York", state: "NY", phone: "123-456-7890", location: [40.717873, -73.995231]})], num_stars: 2, amenities: "Accepts Credit Cards" },
+        {name: "Gild Hall, a Thompson Hotel", place: [new Place({address: "15 Gold Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.707894, -74.007108]})], num_stars: 4, amenities: "Paid Wi-Fi" },
+        {name: "W New York", place: [new Place({address: "123 Washington Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709102, -74.013997]})], num_stars: 3.5, amenities: "Pool, 24 hour Gym, Paid Wi-Fi" },
+        {name: "New York Marriott Downtown", place: [new Place({address: "85 W St at Albany St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.731131, -73.989568]})], num_stars: 3.5, amenities: "24 hour Gym, Paid Wi-Fi" },
+        {name: "Cosmopolitan Hotel", place: [new Place({address: "95 W Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715681, -74.008922]})], num_stars: 3.5, amenities: "Free Wif-Fi" },
+        {name: "Club Quarters", place: [new Place({address: "140 Washington St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709630, -74.013940]})], num_stars: 4, amenities: "Free Wif-Fi" }
+    ],
