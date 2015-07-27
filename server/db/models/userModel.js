d063f3b197666c7c086d9499aabf1653fa97965c
GregAMeyer <gregmeyer888@gmail.com>
2015-07-27 12:44:27 -0400

    added salt stuff to user schema

--- server/db/models/userModel.js
+++ server/db/models/userModel.js
@@ -38,6 +38,30 @@ var userSchema = new mongoose.Schema({
     }
 })
 
+// generateSalt, encryptPassword and the pre 'save' and 'correctPassword' operations
+// are all used for local authentication security.
+var generateSalt = function () {
+    return crypto.randomBytes(16).toString('base64');
+};
+var encryptPassword = function (plainText, salt) {
+    var hash = crypto.createHash('sha1');
+    hash.update(plainText);
+    hash.update(salt);
+    return hash.digest('hex');
+};
+userSchema.pre('save', function (next) {
+    if (this.isModified('password')) {
+        this.salt = this.constructor.generateSalt();
+        this.password = this.constructor.encryptPassword(this.password, this.salt);
+    }
+    next();
+});
+userSchema.statics.generateSalt = generateSalt;
+userSchema.statics.encryptPassword = encryptPassword;
+userSchema.method('correctPassword', function (candidatePassword) {
+    return encryptPassword(candidatePassword, this.salt) === this.password;
+});
+
 module.exports = {
 		User: mongoose.model('User', userSchema)
 }
