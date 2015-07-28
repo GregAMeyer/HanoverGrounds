31b08214259947c833c9eed99476a09e84cca287
GregAMeyer <gregmeyer888@gmail.com>
2015-07-28 11:19:05 -0400

    node works

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,17 +1,17 @@
 'use strict';
 var router = require('express').Router();
 module.exports = router;
-var Product = require('../../db/models/productModel.js');
-var User = require('../../db/models/userModel.js');
+//var Product = require('../../db/models/productModel.js');
+//var User = require('../../db/models/userModel.js');
 
 
 router.use('/members', require('./members'));
 
 //for getting products in home page and other pages
-router.use('/api/products', require('./products'));
+router.use('/products', require('./products'));
 
 //for getting products for sale in dashboard
-router.use('/api/products', require('./dashboard'));
+router.use('/dashboard', require('./dashboard'));
 
 
 // Make sure this is after all of
