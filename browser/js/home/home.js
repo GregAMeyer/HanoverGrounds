d850235f0ae650e6e62bf839e92c82811385753e
Kelsey <kelseysroberts@gmail.com>
2015-07-28 10:26:00 -0400

    home page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,12 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'homeCtrl'
     });
 });
+
+
+app.controller('homeCtrl', function($scope, sliderPics){
+	$scope.images = _.shuffle(sliderPics);
+})
---
+++ browser/js/home/homeFactory.js
@@ -0,0 +1,10 @@
+app.factory('sliderPics', function () {
+    return [
+        'http://www.hdwallpaperscool.com/wp-content/uploads/2015/02/beautiful-coffee-cups-high-resolution-wallpaper-for-desktop-background-download-coffee-images.jpg',
+        'http://wallpaperlepi.com/wp-content/uploads/2014/10/Coffee-Beans-Global-Wallpaper-HD.jpg',
+        'http://www.organicgreencoffeebeans.com/wp-content/uploads/2011/12/Organic-coffee-beans.jpg',
+        'https://hdwallpapers.cat/wallpaper_gray/gray_cup_of_coffee_lovely_beautiful_beans_good_hd-wallpaper-1279345.jpg',
+        'http://gallsource.com/wp-content/uploads/2014/09/Dark-Sky-Coffee-Photos-Free-Download.jpg'
+        
+    ];
+});
