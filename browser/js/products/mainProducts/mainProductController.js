e9e4e6e52286abfaf39e6c6c17d258b3566958f8
Nirmohi <nirmohi0605@gmail.com>
2015-08-04 16:16:25 -0400

    fixed categories on product page

--- browser/js/products/mainProducts/mainProductController.js
+++ browser/js/products/mainProducts/mainProductController.js
@@ -1,40 +1,3 @@
-// app.controller('mainProductCtrl', function($scope, $state, mainProductFactory) {
-//     $scope.categories = [];
-//     mainProductFactory.getAll()
-//         .then(function(products) {
-//             $scope.products = products
-//             return products
-//         })
-
-
-//     .then(function(products) {
-//         $scope.productList = [];
-//         products.forEach(function(product) {
-//             mainProductFactory.getCategories(product._id)
-//                 .then(function(categories) {
-//                     var cats = {};
-//                     for (var key in categories) {
-//                         if (key !== "_id" && key !== "__v") {
-//                             cats[key] = categories[key];
-//                         }
-//                     }
-//                     product.categories = cats;
-//                     $scope.productList.push(product);
-//                     console.log(product.categories);
-//                 })
-//         })
-
-//     })
-
-//     $scope.getDetail = function(product) {
-//         $state.go('detailState', {
-//             id: product._id
-//         });
-//     };
-
-// });
-
-
 app.controller('mainProductCtrl', function($scope, $state, mainProductFactory) {
     $scope.categories = [];
     mainProductFactory.getAll()
@@ -72,17 +35,11 @@ app.controller('mainProductCtrl', function($scope, $state, mainProductFactory) {
         });
     };
 
+    $scope.showCategories = function(product) {
+        if (product.categories.itemCategory === 'Coffee') return product.categories.itemCategory + '/' + product.categories.roast + '/' + product.categories.region;
+        else if (product.categories.itemCategory === 'Hardware') return product.categories.itemCategory + '/' + product.categories.machine;
+    }
+
+
 });
-
-
-
-/*
-
-
-get all products - forEach over all the products to get respective categories
-
-attach correct product to correct category
-
-render
-
-*/
+''
