57c322a148298c94418577d4d76ade96860ec078
GregAMeyer <gregmeyer888@gmail.com>
2015-08-04 23:44:37 -0400

    detail page style

--- browser/js/common/directives/navbar/scrollPosition.js
+++ browser/js/common/directives/navbar/scrollPosition.js
@@ -1,12 +1,12 @@
 app.directive("scroll", function ($window) {
     return function(scope, element, attrs) {
         angular.element($window).bind("scroll", function() {
-             if (this.pageYOffset >= 170) {
+             if (this.pageYOffset >= 150) {
                  scope.boolChangeClass = true;
-                 console.log('Scrolled below header.');
+                 //console.log('Scrolled below header.');
              } else {
                  scope.boolChangeClass = false;
-                 console.log('Header is in view.');
+                 //console.log('Header is in view.');
              }
             scope.$apply();
         });
